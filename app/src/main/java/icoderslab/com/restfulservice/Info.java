e7a5f25cfce268bfd4920307ed97d13778f82788
HarrisDas <harrisdas0@gmail.com>
2017-01-27 17:59:29 +0500

    Initial commit

---
+++ app/src/main/java/icoderslab/com/restfulservice/Info.java
@@ -0,0 +1,33 @@
+package icoderslab.com.restfulservice;
+
+/**
+ * Created by harris on 11/16/2016.
+ */
+public class Info {
+
+    private String id;
+    private String content;
+    private String name;
+    private String num;
+    private String emil;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNum() {
+    return this.num;
+    }
+
+    public String getEmail() {
+    return this.emil;
+    }
+}
